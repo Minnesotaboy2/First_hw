48b216fa081948d7cf69e6720727796d74ee1a64
Andrew Martinek <andrewdmartinek@gmail.com>
2022-03-14 15:17:06 -0500

    added button along with js file

---
+++ script.js
@@ -0,0 +1,4 @@
+var pressedButton = document.getElementsByTagName("button")[0];
+pressedButton.addEventListener("click", function (event) {
+   alert("You have wasted your time, please appreciate the time that you have :).")
+})
